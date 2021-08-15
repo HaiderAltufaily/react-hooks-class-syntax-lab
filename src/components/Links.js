4f818dc033e49113a67c84aa9299196a55c0a407
haider <haiderlek@gmail.com>
2021-08-15 14:03:54 +0300

    commit tree dc12477ab2c68399c4d5e8e7268dfd70505416be

--- src/components/Links.js
+++ src/components/Links.js
@@ -1,13 +1,23 @@
-import React from "react";
-
-function Links(props) {
-  return (
-    <div>
-      <h3>Links</h3>
-      <a href={props.github}>{props.github}</a>
-      <a href={props.linkedin}>{props.linkedin}</a>
-    </div>
-  );
+import React, { Component } from "react";
+class Links extends Component {
+  render() {
+    return (
+      <div>
+        <h3>Links</h3>
+        <a href={this.props.github}>{this.props.github}</a>
+        <a href={this.props.linkedin}>{this.props.linkedin}</a>
+      </div>
+    );
+  }
 }
+// function Links(props) {
+//   return (
+//     <div>
+//       <h3>Links</h3>
+//       <a href={props.github}>{props.github}</a>
+//       <a href={props.linkedin}>{props.linkedin}</a>
+//     </div>
+//   );
+// }
 
 export default Links;
